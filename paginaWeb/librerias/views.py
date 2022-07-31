4d592d0b157c1851ce30ecab5c006c5671f2a7c0
Uriel <uriel_2012_666@hotmail.com>
2022-07-31 14:04:41 -0700

    se configuro una vista la cual muestra una bienvenida simple

--- paginaWeb/librerias/views.py
+++ paginaWeb/librerias/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse #libreria para realizar peticiones
 # Create your views here.
+
+def inici():
+    return HttpResponse("<h1> Bienvenido a Django </h1>")
